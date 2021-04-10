0132c0efbbb27c0e79aa95238cf1134fe5eb7a4f
hirokuni-k <shisenramen@yahoo.co.jp>
2021-04-10 17:21:04 +0900

    いいねのカウント

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
     current_user.likes.create!(post_id: params[:post_id])
-    redirect_back(fallback_location: root_path)
+    @post = Post.find(params[:post_id])
   end
 
   def destroy
-    current_user.likes.find_by(post_id: params[:post_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    current_user.likes.find_by(post_id: @post.id).destroy!
+    @post = Post.find(params[:post_id])
   end
 end
