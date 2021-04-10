41c095d844746ef513bf12839fe4c9c80cfdea1d
hirokuni-k <shisenramen@yahoo.co.jp>
2021-04-10 16:26:07 +0900

    いいね機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   validates :content, presence: true
+
+  def liked_by?(user)
+    likes.find_by(user_id: user.id).present?
+  end
 end
