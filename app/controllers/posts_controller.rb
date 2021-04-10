fcc5d953baf64bf25825e99e9ae56c041ed2c900
hirokuni-k <shisenramen@yahoo.co.jp>
2021-04-10 12:52:57 +0900

    メッセージの編集機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[edit update destroy]
+
   def index
     @posts = Post.order(:id).includes(:user)
   end
@@ -13,20 +15,31 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    @post.update!(post_params)
+    redirect_to @post
   end
 
   def destroy
+    @post.destroy!
+    redirect_to root_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def set_post
+    # 「自分の投稿」の中から URL の :id に対応する投稿を探す
+    # 「他人の投稿」の場合はエラーを出す
+    @post = current_user.posts.find(params[:id])
   end
 end
